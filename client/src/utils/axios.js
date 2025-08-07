04c80b0a1854abbc64dd4b069462641d280e4413
Himani7i <153392456+Himani7i@users.noreply.github.com>
2025-08-07 11:14:32 +0530

    fix bug

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,7 +4,7 @@ let baseURL = process.env.REACT_APP_API_URL|| 'http://localhost:5000';
 if (baseURL.endsWith('/')) baseURL = baseURL.slice(0, -1); 
 
 const API = axios.create({
-  baseURL: `${baseURL}/api`, 
+  baseURL: `${baseURL}`, 
   withCredentials: true,
 });
 export default API;

65be876d192b36a210c30a72a7feb82279273bf6
Himani7i <153392456+Himani7i@users.noreply.github.com>
2025-08-07 20:28:46 +0530

    fix colors

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,18 +12,18 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="p-4 flex justify-between shadow bg-rose-800 sticky top-0 z-50 text-pink-100">
-      <Link to="/" className="text-xl font-bold text-pink-100 hover:text-pink-300">MiniLinkedIn</Link>
+    <nav className="p-4 flex justify-between shadow bg-rose-200 sticky top-0 z-50 text-pink-800">
+      <Link to="/" className="text-xl font-bold text-pink-800 hover:text-pink-300">PostApp</Link>
       <div className="flex items-center gap-4">
         {user ? (
           <>
             <Link to={`/profile/${user._id}`}className="hover:text-pink-400">{user.name}</Link>
-            <button onClick={handleLogout} className="text-sm text-pink-300 hover:text-pink-400">Logout</button>
+            <button onClick={handleLogout} className="text-sm text-pink-400 hover:text-pink-400">Logout</button>
           </>
         ) : (
           <>
-            <Link to="/login" className="hover:text-pink-300">Login</Link>
-            <Link to="/register" className="hover:text-pink-300">Register</Link>
+            <Link to="/login" className="hover:text-pink-400">Login</Link>
+            <Link to="/register" className="hover:text-pink-400">Register</Link>
           </>
         )}
       </div>

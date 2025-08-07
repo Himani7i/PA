3a6533b8fc61ed51c0e270cf8e0b21e493ed6a4b
Himani7i <153392456+Himani7i@users.noreply.github.com>
2025-08-07 11:07:29 +0530

    added vercel url in cors

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['https://pa-zeta-two.vercel.app','http://localhost:3000'], credentials: true }));
 app.use(cookieParser());
 
 // Routes
